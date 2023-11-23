ea7ff7afd651c5181b3f9db2f5db50eae7eb912e
TijsBaeyens <91473368+TijsBaeyens@users.noreply.github.com>
2023-11-23 18:35:58 +0100

    Add files via upload

---
+++ Screens/ErrorHandlePage.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function ErrorHandlePage({ navigation }) {
+  const scanAnotherOne = () => {
+    navigation.navigate('QRPage');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <MaterialIcons name="clear" size={100} color="red" />
+      </View>
+      <Text style={styles.text}>Dit is geen ticket voor deze show</Text>
+      <TouchableOpacity onPress={scanAnotherOne} style={styles.button}>
+        <Text style={styles.buttonText}>Scan volgende QR</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 100,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: 'red',
+  },
+  button: {
+    marginTop: 30,
+    padding: 15,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
